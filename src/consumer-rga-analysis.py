667944486607c868e1998a3ecc3fed0690d75553
Morgan Wall <mkywall3@gmail.com>
2026-06-17 09:19:58 -0700

    use parent project id

--- src/consumer-rga-analysis.py
+++ src/consumer-rga-analysis.py
@@ -64,7 +64,7 @@ def create_sample_dataset(sample_entry, spot, ds, directory, crucible_client, sa
                   dataset_name = f"RGATEY_{ds.dataset_name}_{spot}_{sample_name}",
                   instrument_name = "ALS-BL12012",
                   measurement = "automated_RGA_TEY_run", # TODO - swap to RGA/TEY?
-                  project_id = "10k_perovskites",
+                  project_id = ds.project_id,   # use project_id of the parent
                   data_type = "automated_RGA_TEY_run")
 
     sample_files = glob.glob(os.path.join(directory, f"{sample_name}_*.txt"))
